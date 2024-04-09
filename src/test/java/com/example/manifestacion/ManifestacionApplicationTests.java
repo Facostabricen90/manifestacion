7e02cc8c7340df8fb3e647c94fd90d38f85085e2
“facostabricen90” <frank.acosta6536@ucaldas.edu.co>
2024-04-09 17:29:24 -0500

    primer commit

---
+++ src/test/java/com/example/manifestacion/ManifestacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manifestacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManifestacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
